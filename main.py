99bdeda3512333aeff021e6e53ee2b84a67451d3
Sereres Johnston <sereres@gmail.com>
2020-12-19 09:28:29 -0600

    added tests to cover solving simple expressions between one and three values long

--- main.py
+++ main.py
@@ -8,13 +8,38 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+
 def Calculate(string):
-    val1, op, val2 = string.split(" ")
-    if op == "+":
-        calc = int(val1)+int(val2)
-    elif op == "*":
-        calc = int(val1)*int(val2)
+    formatted_string = string.split(" ")
+    calc = RunCalcs(formatted_string)
     return calc
+
+
+def RunCalcs(formatted_string):
+    if len(formatted_string) == 3:
+        calc = SingleCalc(formatted_string)
+
+    elif len(formatted_string) > 3:
+        calc = SingleCalc(formatted_string)
+        print(calc)
+        print(formatted_string[3:])
+        new_list = [str(calc)]
+        [new_list.append(x) for x in formatted_string[3:]]
+        calc = RunCalcs(new_list)
+
+    else:
+        calc = int(formatted_string[0])
+    return calc
+
+
+def SingleCalc(formatted_string):
+    val1, op, val2, *_ = formatted_string
+    if op == "+":
+        calc = int(val1) + int(val2)
+    elif op == "*":
+        calc = int(val1) * int(val2)
+    return calc
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import unittest
 from main import Calculate
 
+
 class TestAOCDay18(unittest.TestCase):
     def testSmallStringAdd(self):
         calc_result = Calculate("3 + 3")
@@ -11,3 +12,13 @@ class TestAOCDay18(unittest.TestCase):
         calc_result = Calculate("3 * 3")
 
         self.assertEqual(calc_result, 9)
+
+    def testReturnNumber(self):
+        calc_result = Calculate("465")
+
+        self.assertEqual(calc_result, 465)
+
+    def testLongString(self):
+        calc_result = Calculate("3 + 3 * 2")
+
+        self.assertEqual(calc_result, 12)
